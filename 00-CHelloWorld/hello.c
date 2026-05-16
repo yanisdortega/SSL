17c8e09372274aadbdd3e9c7ccd8f2647b8aa51c
unknown <yortega@frba.utn.edu.ar>
2026-05-16 16:15:19 -0300

    cambio

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello, World! \a");
+int main() {
+    printf("Versión de compilador: GCC %d.%d.%d\n",
+           __GNUC__,
+           __GNUC_MINOR__,
+           __GNUC_PATCHLEVEL__);
+
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+
     return 0;
 }
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,7 @@
+1. Compilador seleccionado: GCC MingW64
+2. Versión del compilador: GCC 16.1.0
+3. Versión de C que el compilador compila: C23 
+
+Comandos ejecutados:
+gcc hello.c -o a.exe
+echo Hello, World! | .\hello > output.txt
